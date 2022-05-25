ea2da6ebf32218fb4d145bed13ec0297520d24a2
Suleiman Ibrahim <isuleimanibrahim@gmail.com>
2022-05-25 12:54:40 +0100

    Initial commit

---
+++ src/main/java/org/example/Interface/ProductBookInterface.java
@@ -0,0 +1,7 @@
+package org.example.Interface;
+
+import org.example.models.Customer;
+
+public interface WookBookInterface {
+    String sell(Customer customer);
+}
---
+++ src/main/java/org/example/models/Product.java
@@ -0,0 +1,10 @@
+package org.example.models;
+
+import org.example.Interface.WookBookInterface;
+
+public class WorkBook implements WookBookInterface {
+    @Override
+    public String sell(Customer customer) {
+        return null;
+    }
+}
